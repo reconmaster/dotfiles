729462f153a526cac144dbeb88eaa28905d264ee
Andrew Davis <reconmaster@posteo.net>
2018-04-25 08:38:24 -0400

    Added fix for ssh-agent without gnome keyring
    - Gnome keyring doesn't support the ECC keys, which means I am
      ditching it
    
    - It looks like gpg know has its own agent that can handle gpg keys
      and ssh keys. I'll make that work as that is a suite used by
      everyone who knows what they're doing

--- bashrc.sh
+++ bashrc.sh
@@ -10,3 +10,9 @@
 if [ -f ~/.bashrc_local ]; then
     source ~/.bashrc_local
 fi
+
+
+linux_bash="$HOME/.ssh/service/ssh-agent"
+if [ -e "$linux_bash" ];then
+setsid "$linux_bash" 2>&1 & disown
+fi
